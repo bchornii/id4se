0d33d5937b35cfa0542bea339650f39807b22050
Bohdan Chornii <bchornii@softserveinc.com>
2018-01-24 19:46:43 +0200

    Added console app client to image gallery api

--- IDP/Config.cs
+++ IDP/Config.cs
@@ -163,6 +163,27 @@ namespace IDP
                         IdentityServerConstants.StandardScopes.Profile,
                         "imagegalleryapi"
                     }
+                },
+
+                // app client
+                new Client
+                {
+                    ClientId = "imagegalleryappclient",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("imagegalleryappclientsecret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes =
+                    {
+                        "imagegalleryapi"
+                    }
                 }
             };
         }
